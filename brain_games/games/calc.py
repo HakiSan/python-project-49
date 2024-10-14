43551fd82a46cfe1ee3cf8b08216c323e5e40344
HakiSan <i@hakisan.ru>
2024-10-14 10:13:20 +0300

    corrections #2, fix calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,13 +1,23 @@
 import random
+from operator import sub, add, mul
 
 
 TASK = 'What is the result of the expression?'
 
 
+def calculate(sign):
+    if sign == '+':
+        return add
+    elif sign == '*':
+        return mul
+    elif sign == '-':
+        return sub
+
+
 def get_question():
     operand1 = random.randint(1, 100)
     operand2 = random.randint(1, 10)
-    operator = random.choice(["+", "-", "*"])
-    question = f'{operand1} {operator} {operand2}'
-    correct_answer = str(eval(question))
+    sign = random.choice(["+", "-", "*"])
+    question = f'{operand1} {sign} {operand2}'
+    correct_answer = str(calculate(sign)(operand1, operand2))
     return question, correct_answer
